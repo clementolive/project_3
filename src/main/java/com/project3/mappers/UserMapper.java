db73ce81e728d5892231d581b58cf8ab6c39ecca
Achmoye <achmoye@gmail.com>
2023-03-15 13:33:27 +0100

    Documentation works with OpenAPI at http://localhost:8080/swagger-ui/index.html

--- src/main/java/com/project3/mappers/UserMapper.java
+++ src/main/java/com/project3/mappers/UserMapper.java
@@ -1,22 +1,15 @@
 package com.project3.mappers;
-
 import com.project3.dtos.UserDTO;
 import com.project3.entities.User;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.factory.Mappers;
-
-import java.util.Optional;
 
 @Mapper(componentModel = "spring")
 public interface UserMapper {
 
-   // UserMapper INSTANCE = Mappers.getMapper(UserMapper.class);
-
     @Mapping(target="email", source="user.email")
     @Mapping(target="name", source="user.name")
     UserDTO userToUserDTO(User user);
-
-
+    
     User userDTOToUser(UserDTO userDTO);
 }
